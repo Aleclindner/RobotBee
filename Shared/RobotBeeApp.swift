911502ebbe8e748fe77e16ec36b470dd21d89f4f
Alec Lindner <aleclindner@gmail.com>
2022-09-06 16:04:15 -0700

    first commit

---
+++ Shared/RobotBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotBeeApp.swift
+//  Shared
+//
+//  Created by Alec Lindner on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct RobotBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
